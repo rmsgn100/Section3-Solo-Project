428422db97d9551b25e8b539c93d9b3de3ad0fb1
최근후 <choegeunhu@choegeunhuui-MacBookPro.local>
2020-11-30 01:47:46 +0900

    sqlite db

--- migrations/versions/d06d86af9339_.py
+++ migrations/versions/d06d86af9339_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: bb88adecc0de
+Revision ID: d06d86af9339
 Revises: 
-Create Date: 2020-11-29 23:56:44.753187
+Create Date: 2020-11-30 01:14:13.215404
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'bb88adecc0de'
+revision = 'd06d86af9339'
 down_revision = None
 branch_labels = None
 depends_on = None
--- twit_app/__init__.py
+++ twit_app/__init__.py
@@ -26,4 +26,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)
+    app.run()
